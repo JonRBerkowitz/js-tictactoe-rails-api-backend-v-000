23cdffea08f0ba51ab72f90857b75dc673887d0e
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-03-01 19:41:18 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,8 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
+    state = ["", "", "", "", "", "" ,"", "", ""]
+    @game = Game.new
   end
 
 end
