123ffcf24d634e739055ed0dbe1d2d14b5f19216
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-03-01 19:43:21 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,8 @@ class GamesController < ApplicationController
   # Add your GamesController code here
   def create
     state = ["", "", "", "", "", "" ,"", "", ""]
-    @game = Game.new
+    @game = Game.new(state: state)
+    render json: @game, status: 201
   end
 
 end
